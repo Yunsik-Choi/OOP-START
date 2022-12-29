9d5661eb93e132e2a6eea71dfdc469298ecd3458
Yunsik-Choi <choiys0311@gmail.com>
2022-12-29 11:22:35 +0900

    fix : exception 메시지 수정

--- src/main/java/service/OrderService.java
+++ src/main/java/service/OrderService.java
@@ -8,7 +8,7 @@ public class OrderService {
     public void order(Customer customer, Product product) {
         // 재고 감소 로직
         if (product.getStock() < 1) {
-            throw new RuntimeException("재고가 1개 이상일 때만 주문할 수 있습니다.");
+            throw new RuntimeException("재고가 부족합니다.");
         }
         int remainStock = product.getStock() - 1;
         product.setStock(remainStock);
