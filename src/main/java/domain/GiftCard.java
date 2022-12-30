87b412b5f3acd09b0dc773b02c8b33e57b9464bf
Yunsik-Choi <choiys0311@gmail.com>
2022-12-31 02:50:53 +0900

    feat : 상속 실습 완료

--- src/main/java/domain/GiftCard.java
+++ src/main/java/domain/GiftCard.java
@@ -1,20 +1,8 @@
 package domain;
 
-public class GiftCard implements Money {
-    private int value;
+public class GiftCard extends Point {
 
     public GiftCard(int value) {
-        this.value = value;
-    }
-
-    public void minus(int price) {
-        int originBalance = value;
-        int productPrice = price;
-        int remainBalance = originBalance - productPrice;
-
-        if (remainBalance < 0) {
-            throw new RuntimeException("잔액이 부족합니다.");
-        }
-        value = remainBalance;
+        super(value);
     }
 }
--- src/main/java/domain/Point.java
+++ src/main/java/domain/Point.java
@@ -1,13 +1,20 @@
 package domain;
 
+import java.time.LocalDateTime;
+
 public class Point implements Money {
     private int value;
+    private LocalDateTime expiredDate;
 
     public Point(int value) {
         this.value = value;
+        this.expiredDate = LocalDateTime.now().plusMonths(3L);
     }
 
     public void minus(int price) {
+        if (expiredDate.isBefore(LocalDateTime.now())) {
+            throw new RuntimeException("포인트가 만료됐습니다.");
+        }
         int originBalance = value;
         int productPrice = price;
         int remainBalance = originBalance - productPrice;
